9a5bdb04d5126e9d90db77fa565eafe22dcfb217
hmaraegor <hmaraegor@gmail.com>
2020-08-18 21:18:03 +0300

    Initial Commit

---
+++ WeekWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeekWeather
+//
+//  Created by Egor on 18/08/2020.
+//  Copyright © 2020 Egor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
